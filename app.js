093868bf5add0405b3167cfc3689e24cc686615d
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-08-13 22:55:37 +0530

    Woodlot implemented

--- app.js
+++ app.js
@@ -3,11 +3,16 @@
 const express = require('express'),
 	app = express(),
 	constants = require('./lib/constants'),
-	path = require('path');
+	path = require('path'),
+	woodlot = require('woodlot').middlewareLogger;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(woodlot({
+    streams: ['./logs/app.log']
+}));
 
 app.get('/', (req, res) => res.render('index'));
 
