aedf4ec0611669fb3214552327ebbd214aa5aca0
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-08-28 02:54:51 +0530

    Maps initial addition

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.constants = constants;
+
 app.use(woodlot({
     streams: ['./logs/app.log'],
     stdout: true,
@@ -20,6 +22,8 @@ app.use(woodlot({
 }));
 
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/travelog', (req, res) => res.render('travelog'));
 
 app.listen(constants.PORT, () => {
 	console.log(`Server running on port : ${constants.PORT}`)
