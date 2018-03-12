077f0da352a61a51845a425afe8d4dd317bcb5d7
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-03-12 21:36:44 +0530

    Website updated

--- data.js
+++ data.js
@@ -1,19 +1,30 @@
 module.exports = {
 	talks: [{
+		title: 'Houdini, what lies ahead',
+		urls: {
+			slides: 'https://speakerdeck.com/arunmichaeldsouza/houdini-what-lies-ahead-jsconf-iceland-2018',
+			video: 'https://www.youtube.com/watch?v=3Nv9muOkb6k&t=5610s'
+		},
+		location: 'Reykjavik, Iceland',
+		event: {
+			name: 'JSConf Iceland 2018',
+			url: 'https://2018.jsconf.is/'
+		}
+	}, {
 		title: 'The Era of Module Bundlers',
 		urls: {
-			slides: 'http://bit.ly/the-era-of-module-bundlers-gdgdevfest2017',
-			video: 'http://bit.ly/gdg-2017-module-bundlers'
+			slides: 'https://speakerdeck.com/arunmichaeldsouza/the-era-of-module-bundlers-gdg-dev-fest-delhi-2017',
+			video: 'https://www.facebook.com/gdgnewdelhi/videos/871843672986750/'
 		},
 		location: 'Investopad, Gurugram',
 		event: {
 			name: 'GDG Dev Fest New Delhi 2017',
-			url: 'http://bit.ly/gdg-devfest-2017'
+			url: 'https://www.facebook.com/events/529964404062042/'
 		}
 	}, {
 		title: 'Getting Started with React 16',
 		urls: {
-			slides: 'http://bit.ly/getting-started-with-react-16',
+			slides: 'https://speakerdeck.com/arunmichaeldsouza/getting-started-with-react-16',
 			video: ''
 		},
 		location: 'AdPushup Inc, New Delhi',
@@ -24,13 +35,13 @@ module.exports = {
 	}, {
 		title: 'The Era of Module Bundlers',
 		urls: {
-			slides: 'http://bit.ly/the-era-of-module-bundlers-jsconfbe2017',
-			video: 'http://bit.ly/2sKN8Fl'
+			slides: 'https://speakerdeck.com/arunmichaeldsouza/the-era-of-module-bundlers-jsconf-belgium-2017',
+			video: 'https://www.youtube.com/watch?v=umqy8Fu3j0Q'
 		},
 		location: 'Brugge, Belgium',
 		event: {
 			name: 'JSConf Belgium 2017',
-			url: 'http://bit.ly/jsconf-be-2017'
+			url: 'https://www.jsconf.be'
 		}
 	}]
 };
