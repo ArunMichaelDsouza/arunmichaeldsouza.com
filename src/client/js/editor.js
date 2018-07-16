fd8b15b82c90caa8279eb60cfa3a391e12a87934
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-07-17 04:07:54 +0530

    Default editor content set

--- src/client/js/editor.js
+++ src/client/js/editor.js
@@ -6,6 +6,7 @@ const $ = require('jquery');
 require('medium-editor-insert-plugin')($);
 require('medium-editor-custom-html');
 const EmbedButtonExtension = require('medium-editor-embed-button/src/js/medium-editor-embed-button');
+const defaultHtml = '<h1>Title goes here</h1><h2><span style="font-weight: normal;">Subtitle goes here</span></h2><p class=""><br></p><p class="">Content starts here..</p><div class="medium-insert-buttons" contenteditable="false" style="left: 30px; top: 121.781px; display: none;"> <button class="medium-insert-buttons-show" type="button"><span>+</span></button> <ul class="medium-insert-buttons-addons" style="display: none"> <li><button data-addon="images" data-action="add" class="medium-insert-action" type="button"><span class="fa fa-camera"></span></button></li><li><button data-addon="embeds" data-action="add" class="medium-insert-action" type="button"><span class="fa fa-youtube-play"></span></button></li></ul></div>';
 
 const editor = new MediumEditor('.editor', {
     buttonLabels: 'fontawesome',
@@ -48,3 +49,7 @@ const editor = new MediumEditor('.editor', {
 $('.editor').mediumInsert({
     editor: editor
 });
+
+editor.setContent(defaultHtml, 0);
+
+window.editor = editor;
