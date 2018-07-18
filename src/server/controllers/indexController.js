46adb7508920084a7e4d8a7ed0081bc6a22a0b79
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-07-19 02:53:05 +0530

    Blog view added

--- src/server/controllers/indexController.js
+++ src/server/controllers/indexController.js
@@ -1,12 +1,22 @@
 // Index controller
 
 const router = require('express').Router({ mergeParams: false }),
-    Talk = require('../models/Talk');
+    Talk = require('../models/Talk'),
+    Blog = require('../models/Blog');
 
 router
     .get('/', (req, res) => {
         return Talk.find().sort({ eventDate: -1 })
             .then(talks => res.render('index', { talks }))
+            .catch(err => res.status(500).send('Some error occurred'));
+    })
+    .get('/blog/:blogUrl', (req, res) => {
+        return Blog.findOne({ url: req.params.blogUrl })
+            .then(blog => {
+                const { title } = blog;
+
+                return res.render('blog', { title });
+            })
             .catch(err => res.status(500).send('Some error occurred'));
     })
     .get('/travelog', (req, res) => res.render('travelog'));
