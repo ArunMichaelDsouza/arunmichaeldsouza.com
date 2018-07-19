7d25cde79e1f70623ad295a22816a30821af2cb5
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-07-19 14:36:42 +0530

    Check for unpublished blogs added

--- src/server/controllers/indexController.js
+++ src/server/controllers/indexController.js
@@ -13,9 +13,13 @@ router
     .get('/blog/:blogUrl', (req, res) => {
         return Blog.findOne({ url: req.params.blogUrl })
             .then(blog => {
-                const { title: blogTitle, date, content, url } = blog;
+                const { title: blogTitle, date, content, url, published } = blog;
 
-                return res.render('blog', { blogTitle, date, content, url });
+                if (published) {
+                    return res.render('blog', { blogTitle, date, content, url });
+                }
+
+                return res.send('Blog not published yet!');
             })
             .catch(err => res.status(500).send('Some error occurred'));
     })
