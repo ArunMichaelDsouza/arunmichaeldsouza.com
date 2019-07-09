c8b7dda3011386cba16da87772b64211fde43f15
ArunMichaelDsouza <amdsouza92@gmail.com>
2019-07-10 00:20:19 +0530

    Mongoose settings updated to remove deprecation warnings

--- src/server/lib/db.js
+++ src/server/lib/db.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 });
 
 module.exports = mongoose.connection;
