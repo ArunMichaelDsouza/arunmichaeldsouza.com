c4b6336d3fbc732a68e49048272eb3a0bbfbfc6b
Arun Michael Dsouza <amdsouza92@gmail.com>
2022-03-20 17:51:54 -0400

    Mongo connection fix

--- src/server/lib/db.js
+++ src/server/lib/db.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
-mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}?directConnection=true`, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
