c8922b10b4294e7d3766e910461acd6e1f7c2a94
Arun Michael Dsouza <amdsouza92@gmail.com>
2022-03-23 17:32:33 -0400

    Sass styling added

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,36 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <>
-      <header className="header">
-        <nav className="nav">
-          <Link href="/">
-            <a>My Blog</a>
-          </Link>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
+    <header>
+      <div className="text-center">
+        <nav>
+          <h1 className="page-primary-title">
+            <Link href="/">
+              <a>Arun Michael Dsouza</a>
+            </Link>
+          </h1>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/talks">
+                <a>Talks</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/open-source">
+                <a>Open Source</a>
+              </Link>
+            </li>
+          </ul>
         </nav>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
