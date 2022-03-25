32af3be87a9279eab1c5cfcac270bd63437dcb59
Arun Michael Dsouza <amdsouza92@gmail.com>
2022-03-24 23:19:57 -0400

    Minor changes

---
+++ public/js/disqus.js
@@ -0,0 +1,11 @@
+var disqus_config = function () {
+  this.page.url = window.location.href;
+  this.page.identifier = '';
+};
+(function () {
+  var d = document,
+    s = d.createElement('script');
+  s.src = 'https://arunmichaeldsouza.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
