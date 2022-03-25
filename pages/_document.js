5843902c7ab570440289c8a89d578afa919fee53
Arun Michael Dsouza <amdsouza92@gmail.com>
2022-03-24 22:21:24 -0400

    GA script added

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,17 @@ export default function Document() {
       <Head>
         <link rel="preload" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" as="style" />
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103805032-1" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '[UA-103805032-1]', { page_path: window.location.pathname });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
