f4651bd4931d67358dd63432f5a1b4ed48abe917
Arun Michael Dsouza <amdsouza92@gmail.com>
2022-03-24 22:27:42 -0400

    GA script added

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,23 @@ export default function Document() {
       <Head>
         <link rel="preload" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" as="style" />
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103805032-1" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '[UA-103805032-1]', { page_path: window.location.pathname });
+              (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+              })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+              
+              ga('create', 'UA-103805032-1', 'auto');
+              ga('send', 'pageview');
             `,
           }}
         />
